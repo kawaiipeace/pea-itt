81edff36b9fb4f87abe4381bbccb9ee57a04ba0d
piyathida2310 <18987m210@gmail.com>
2025-06-16 14:37:44 +0700

    แก้หน้าuserrouter

--- backend/src/modules/user/userRoutes.ts
+++ backend/src/modules/user/userRoutes.ts
@@ -1,9 +1,9 @@
 import express from 'express';
-import { getAllUsers, getUserById } from '../controllers/userController';
+import { getAllUsers, getUserById } from '../user/userController';
 
 const router = express.Router();
 
 router.get('/', getAllUsers);
-router.get('/:id', getUserById);
+router.get('/:id', getUserById); 
 
 export default router;
