eb26b621f573acc969b9241b555a044007e3131a
Apiwat <mrapiiwat@gmail.com>
2025-06-16 13:57:28 +0700

    Refactor backend structure: update dependencies, reorganize imports, and remove unused user controller and logger middleware.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,13 @@
-import express from 'express';
-import userRoutes from './routes/userRoutes';
-import logger from './middleware/logger';
+import express from "express";
+import userRoutes from "./modules/user/userRoutes";
+import logger from "./common/middleware/logger";
 
 const app = express();
 const port = 10001;
 
 app.use(express.json());
 app.use(logger); // custom middleware
-app.use('/users', userRoutes); // router
+app.use("/users", userRoutes); // router
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
