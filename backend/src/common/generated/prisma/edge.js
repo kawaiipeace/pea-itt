63da6a9961a70bd8744477aca51dbca43052214b
kawaiipeace <kawaii.peace.kun@gmail.com>
2025-06-17 16:39:36 +0700

    update docker and prisma

--- backend/src/common/generated/prisma/edge.js
+++ backend/src/common/generated/prisma/edge.js
@@ -216,7 +216,8 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null
+    "rootEnvPath": null,
+    "schemaEnvPath": "../../../../.env"
   },
   "relativePath": "../../../../prisma",
   "clientVersion": "6.9.0",
@@ -225,7 +226,7 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": true,
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
--- backend/src/common/generated/prisma/index.js
+++ backend/src/common/generated/prisma/index.js
@@ -217,7 +217,8 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null
+    "rootEnvPath": null,
+    "schemaEnvPath": "../../../../.env"
   },
   "relativePath": "../../../../prisma",
   "clientVersion": "6.9.0",
@@ -226,7 +227,7 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": true,
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
