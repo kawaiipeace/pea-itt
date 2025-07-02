a7c8427eee234007a2004ccf815f4b20f6f89299
Sukhatai09 <sukhatai7331@gmail.com>
2025-07-02 10:56:17 +0700

    ปรับแต่ง card

--- frontend/components/card.tsx
+++ frontend/components/card.tsx
@@ -1,21 +1,20 @@
 import React from "react";
 import useAuthStore from "@/store/authStore";
+import IconUsers from "@/components/icon/icon-users";
 
 const Card = ({ student }: any) => {
   const user = useAuthStore((s) => s.user);
   return (
     <div
       key={student.id}
-      className="flex flex-col items-center rounded-2xl bg-white p-4 text-center shadow transition hover:shadow-md"
+      className="flex flex-col items-center rounded-2xl border border-transparent bg-white p-4 text-center
+        shadow transition-colors hover:border-[#B10073] hover:bg-[#F7E3F0] hover:shadow-md
+        dark:border-gray-900 dark:bg-gray-900 dark:text-[#506690] dark:hover:border-white
+        dark:hover:bg-gray-900"
     >
-      <div
-        className="flex h-20 w-20 items-center justify-center rounded-full bg-gray-100"
-        // style={{
-        //   backgroundImage: `url('${process.env.NEXT_PUBLIC_API_URL}user/${user?.student_profile.id}/picture')`,
-        //   backgroundSize: "cover",
-        //   backgroundPosition: "center",
-        // }}
-      ></div>
+      <div className="flex h-20 w-20 items-center justify-center rounded-full bg-gray-100">
+        <IconUsers className="shrink-0 group-hover:!text-primary" />
+      </div>
       <div className="mt-3 text-base font-semibold md:text-lg">
         {student.name}
       </div>
